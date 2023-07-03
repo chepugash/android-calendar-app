a05be25edfef3cc1bcee9529be539b8a2a29ed6f
chepugash <chepugash@yahoo.com>
2023-07-03 03:37:35 +0300

    detail screen ui

--- app/src/main/java/com/practice/calendar/ui/screen/CalendarScreen.kt
+++ app/src/main/java/com/practice/calendar/ui/screen/CalendarScreen.kt
@@ -2,14 +2,11 @@
 
 package com.practice.calendar.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -20,14 +17,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.practice.calendar.R
-import com.practice.calendar.ui.TaskLiteEntity
-import com.practice.calendar.ui.tasks
 import com.practice.calendar.ui.theme.CalendarTheme
 import com.vanpra.composematerialdialogs.MaterialDialog
 import com.vanpra.composematerialdialogs.datetime.date.datepicker
@@ -36,14 +32,15 @@ import java.time.LocalDate
 
 @Composable
 fun CalendarScreen() {
-    Column (
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
         CalendarToolbar()
-        TaskTable(tasks = tasks)
+//        TaskTable(tasks = tasks)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarToolbar() {
     TopAppBar(
@@ -53,8 +50,10 @@ fun CalendarToolbar() {
         navigationIcon = {
             Icon(
                 painterResource(id = R.drawable.ic_calendar),
-                contentDescription = "toolbar calendar icon")
-        }
+                contentDescription = "toolbar calendar icon",
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            )
+        },
     )
 }
 
@@ -68,7 +67,10 @@ fun DatePicker() {
         TextButton(onClick = {
             dateDialogState.show()
         }) {
-            Text(text = pickedDate.toString())
+            Text(
+                text = pickedDate.toString(),
+                fontSize = 24.sp
+            )
         }
     }
     MaterialDialog(
@@ -84,35 +86,6 @@ fun DatePicker() {
         ) {
             pickedDate = it
         }
-    }
-}
-
-@Composable
-fun TaskTable(tasks: List<TaskLiteEntity>) {
-    LazyColumn(
-        modifier = Modifier.fillMaxHeight()
-    ) {
-        items(tasks.size) {
-            TaskItem(task = tasks[it])
-        }
-    }
-}
-
-@Composable
-fun TaskItem(task: TaskLiteEntity) {
-    Row(
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = task.time,
-            modifier = Modifier.padding(end = 16.dp)
-        )
-        Text(text = task.name)
-
     }
 }
 
--- app/src/main/java/com/practice/calendar/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/practice/calendar/ui/screen/DetailScreen.kt
@@ -1,20 +1,151 @@
 package com.practice.calendar.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.practice.calendar.R
 import com.practice.calendar.ui.theme.CalendarTheme
+import java.time.LocalDateTime
 
 @Composable
 fun DetailScreen() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        DetailToolbar(title = "Call John")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            EventTitle(title = "Call John")
+            Spacer(modifier = Modifier.padding(8.dp))
+            EventTime(start = "08:00", finish = "09:00")
+            Spacer(modifier = Modifier.padding(8.dp))
+            EventDate(date = "28 июня, 2023")
+            Spacer(modifier = Modifier.padding(8.dp))
+            EventDescription(desc = "Call John for booking a flat on birthday party")
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailToolbar(title: String) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 24.sp
+            )
+        },
+        navigationIcon = {
+            Icon(
+                painterResource(id = R.drawable.ic_back),
+                contentDescription = "toolbar back icon",
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            )
+        },
+    )
+}
 
+@Composable
+fun EventTitle(title: String) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+    ) {
+        Text(
+            text = title,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun EventTime(start: String, finish: String) {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Icon(
+            painterResource(id = R.drawable.ic_time),
+            contentDescription = "toolbar calendar icon in details",
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .align(Alignment.CenterVertically)
+        )
+        Text(
+            text = start,
+            fontSize = 24.sp,
+        )
+        Text(
+            text = "-",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+        )
+        Text(
+            text = finish,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Composable
+fun EventDate(date: String) {
+    Row {
+        Icon(
+            painterResource(id = R.drawable.ic_calendar),
+            contentDescription = "toolbar calendar icon in details",
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .align(Alignment.CenterVertically)
+        )
+        Text(
+            text = date,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Composable
+fun EventDescription(desc: String) {
+    Box {
+        Text(
+            text = desc,
+            fontSize = 18.sp)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DetailPreview() {
     CalendarTheme {
-        CalendarScreen()
+        DetailScreen()
     }
 }
